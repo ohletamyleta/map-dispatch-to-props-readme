cdb06be304ba703b988a8959384b36741cc0a413
ohletamyleta <ohletamyleta@gmail.com>
2021-04-28 10:33:15 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,35 @@ import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
-  }
+  handleOnClick = (event) => {
+    this.props.addItem();
+  };
 
   render() {
+    // debugger;
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
+        <button onClick={this.handleOnClick}>Click</button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     items: state.items
+//   };
+// };
 
-export default connect(mapStateToProps)(App);
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addItem: () => {
+//       dispatch(addItem());
+//     },
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default connect((state) => ({ items: state.items }), { addItem })(App);
